71122681d9be1e770709e6998e95e092b93165d0
Halocline <mdglissmann@gmail.com>
2019-03-08 16:29:45 -0700

    Adding react framework

---
+++ detectImage.js
@@ -0,0 +1,26 @@
+const darknet = require('@moovel/yolo');
+const fs = require('fs')
+
+darknet.detectImage({
+  cfg: './cfg/yolo.cfg',
+  weights: './yolo.weights',
+  data: './cfg/coco.data',
+  image: './data/dog.jpg',
+  thresh: 0.24, // optional, default: 0.24
+  hierThresh: 0.5, // optional, default: 0.5,
+}, (modified, original, detections, dimensions) => {
+  handleDetections(detections)
+})
+
+const handleDetections = (detections) => {
+  console.log('Detections:', detections)
+  console.log( detections.length )
+  const data = JSON.stringify(detections, null, 3)
+  console.log(data);
+
+  fs.writeFile('detections.json', data, err => {
+    if (err) throw err
+    console.log('The file has been saved!')
+  })
+
+}
