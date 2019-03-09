d62608670186211d3fcaa150da716c68238c0241
Halocline <mdglissmann@gmail.com>
2019-03-08 17:02:24 -0700

    Stubbing out Detections component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Detections from './detections';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <header></header>
+        <Detections />
+        <footer>
+          <div>I am a footer.</div>
+        </footer>
       </div>
     );
   }
---
+++ src/detections/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Detections extends React.Component {
+  render() {
+    return (
+      <div className="Detections">
+        <div>
+          <h2>Detections</h2>
+        </div>
+        <div>Detected Objects</div>
+        <div>Detection Image</div>
+      </div>
+    )
+  }
+}
+
+export default Detections;
